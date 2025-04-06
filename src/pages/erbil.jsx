ffea62b0debfa862623e3798609f23f6dd2efa6f
Diyary <arazomar500@gmail.com>
2025-04-06 21:15:33 +0300

    version 2

--- src/pages/erbil.jsx
+++ src/pages/erbil.jsx
@@ -1,11 +1,9 @@
 const Erbil = () => {
   return (
     <div>
-      <header>
-        <div className="title">
-          <h1>Erbil</h1>
-        </div>
-      </header>
+      <div className="title">
+        <h1 className="flex min-h-screen text-center text-green-300">Erbil</h1>
+      </div>
     </div>
   );
 };
